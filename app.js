b85653648ba6592035421b55734bf87c370f5c7c
ahnsucheol <josephscahn@gmail.com>
2022-08-09 15:48:44 +0900

    Add: 유저 회원가입 엔드포인트 구현

---
+++ app.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const express = require("express");
+const { createUser } = require("./user");
+
+const app = express();
+app.use(express.json());
+
+const users = [
+  {
+    id: 1,
+    name: "Rebekah Johnson",
+    email: "Glover12345@gmail.com",
+    password: "123qwe",
+  },
+  {
+    id: 2,
+    name: "Fabian Predovic",
+    email: "Connell29@gmail.com",
+    password: "password",
+  },
+];
+
+const posts = [
+  {
+    id: 1,
+    title: "간단한 HTTP API 개발 시작!",
+    content: "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
+    userId: 1,
+  },
+  {
+    id: 2,
+    title: "HTTP의 특성",
+    content: "Request/Response와 Stateless!!",
+    userId: 1,
+  },
+];
+
+app.get("/signup", createUser);
+
+const server = http.createServer(app);
+
+server.listen(8000, () => {
+  console.log("Server is listening on PORT 8000");
+});
